05a83311b54f15ecf1b140055f067e0dc10bd2a2
arun-experion <arun.ks@experionglobal.com>
2024-09-03 12:30:30 +0530

    latest code push

---
+++ app/ProductImage.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+use Storage;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductImage extends Model
+{
+    protected $appends = ['image_url'];
+    protected $hidden = [
+        'created_at', 'updated_at',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     * 
+     */
+    protected $fillable = ['product_id', 'image', 'created_at', 'updated_at'];    
+     
+    public function getImageUrlAttribute() {
+
+        return  Storage::disk('public')->url('products/') . $this->image;
+
+    }
+}
